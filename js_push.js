8a7e025d4b64152432cbf53dac1ed8740072648d
wangbaoqiang-lixiaolu <59414526+wangbaoqiang-lixiaolu@users.noreply.github.com>
2023-06-23 03:40:11 +0800

    Update js_push.js

--- js_push.js
+++ js_push.js
@@ -1,10 +1,17 @@
 /*百度主动实时自动推送代码开始*/
-(function(){ 
-var bp = document.createElement('script'); 
-bp.src = '//push.zhanzhang.baidu.com/push.js'; 
-var s = document.getElementsByTagName("script")[0]; 
-s.parentNode.insertBefore(bp, s); 
-})(); 
+!function() {
+    var e = /([http|https]:\/\/[a-zA-Z0-9\_\.]+\.baidu\.com)/gi
+      , r = window.location.href
+      , t = document.referrer;
+    if (!e.test(r)) {
+        var o = "https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif";
+        t ? (o += "?r=" + encodeURIComponent(document.referrer),
+        r && (o += "&l=" + r)) : r && (o += "?l=" + r);
+        var i = new Image;
+        i.src = o
+    }
+}(window);
+
 /*百度主动实时自动推送代码束*/
 
 /*<!--360自动收录--> */
